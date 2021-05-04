6a496680b55c773b232e478e2aaac8cd64edfdd7
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-05-04 13:06:01 +0200

    change js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
-console.log("Witam developerów!");
+{
+  const welcome = () => {
+    console.log("Witam developerów!");
+  }
 
-let buttonHeader = document.querySelector(".js-main__buttonHeader");
-let header = document.querySelector(".js-header");
+  const toggleTheme = () => {
+    const body = documentElement;
+    const buttonTheme = document.querySelector(".js-themeName");
 
-buttonHeader.addEventListener("click", () => {
-  header.classList.toggle("header__hideHeader");
-  buttonHeader.innerText = buttonHeader.classList.contains("header")
-    ? "Schowaj nagłówek" : "Pokaż nagłówek";
-});
+    body.classList.toggle("dark");
+    buttonTheme.innerText = body.classList.contains("dark") ? "Jasny motyw" : "Ciemny motyw";
+  }
 
-let buttonTheme = document.querySelector(".js-main__buttonTheme");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-themeName")
+  const hideHeader = () => {
+    const buttonHeader = document.querySelector(".js-main__buttonHeader");
+    const header = document.querySelector(".js-header");
 
-buttonTheme.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  buttonTheme.innerText = body.classList.contains("dark")
-    ? "Jasny motyw" : "Ciemny motyw";
-});
+    
+    header.classList.toggle("header__hideHeader");
+    buttonHeader.innerText = buttonHeader.classList.contains("header")
+      ? "Schowaj nagłówek" : "Pokaż nagłówek";
+
+  }
+
+  const init = () => {
+    const buttonTheme = document.querySelector(".js-main__buttonTheme");
+    buttonTheme.addEventListener("click", toggleTheme);
+    buttonHeader.addEventListener("click", hideHeader);
+    welcome();
+  };
+
+  init();
+}
